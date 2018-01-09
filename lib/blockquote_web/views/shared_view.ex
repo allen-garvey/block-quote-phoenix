b731f76c1ac0c76da17bb9dee4b692081330ba81
Allen Garvey <allengarvey@hotmail.com>
2018-01-09 19:32:29 +0000

    Add shared form templates

--- lib/blockquote_web/views/shared_view.ex
+++ lib/blockquote_web/views/shared_view.ex
@@ -45,4 +45,32 @@ defmodule BlockquoteWeb.SharedView do
   	def pad_date_digit(digit) do
   		Integer.to_string(digit) |> String.pad_leading(2, ["0"])
   	end
+  	
+  	
+  	@doc """
+  	Used for forms to determine if required
+  	"""
+  	def field_required?(field_atom, required_fields) do
+  		required_fields |> Enum.member?(field_atom)
+  	end
+  	
+  	def field_required_label_class(true) do
+  		"required"
+  	end
+  	
+  	def field_required_label_class(false) do
+  		""
+  	end
+  	
+	def form_item_label(f, field, required_fields) do
+		label f, field, class: "control-label " <> field_required_label_class(field_required?(field, required_fields))
+	end
+	
+	def form_input(f, :string, field, required_fields, nil) do
+		text_input(f, field, class: "form-control", required: field_required?(field, required_fields))
+	end
+	
+	def form_input(f, :text, field, required_fields, nil) do
+		textarea(f, field, class: "form-control", required: field_required?(field, required_fields))
+	end
 end
