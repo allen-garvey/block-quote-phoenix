77b9b56b0895fe86ad289c150cc79a7b222b39dc
Allen Garvey <allen.garvey@gmail.com>
2018-01-29 16:55:55 -0500

    Fix indentation

--- lib/blockquote_web/views/shared_view.ex
+++ lib/blockquote_web/views/shared_view.ex
@@ -101,37 +101,37 @@ defmodule BlockquoteWeb.SharedView do
 		select(f, field, items, class: "form-control", required: field_required?(field, required_fields))
 	end
 
-    def render("new.html", assigns) do
-        conn = assigns[:conn]
-        item_name_singular = assigns[:item_name_singular]
-        
-        assigns = Map.merge(assigns, 
-            %{
-                title: "Add " <> item_name_singular,
-                back_link_title: "All " <> naive_pluralize(item_name_singular),
-                back_link_path: path_for_item(conn, item_name_singular, :index),
-                action: path_for_item(conn, item_name_singular, :create),
-            }
-        )
-        
-        render "form_page.html", assigns
-    end
+  def render("new.html", assigns) do
+      conn = assigns[:conn]
+      item_name_singular = assigns[:item_name_singular]
+      
+      assigns = Map.merge(assigns, 
+          %{
+              title: "Add " <> item_name_singular,
+              back_link_title: "All " <> naive_pluralize(item_name_singular),
+              back_link_path: path_for_item(conn, item_name_singular, :index),
+              action: path_for_item(conn, item_name_singular, :create),
+          }
+      )
+      
+      render "form_page.html", assigns
+  end
 
-    def render("edit.html", assigns) do
-        conn = assigns[:conn]
-        item_name_singular = assigns[:item_name_singular]
-        item = assigns[:item]
-        item_display_name = assigns[:item_display_name]
-        
-        assigns = Map.merge(assigns, 
-            %{
-                title: "Edit " <> item_display_name,
-                back_link_title: "Back to " <> item_display_name,
-                back_link_path: path_for_item(conn, item_name_singular, :show, item),
-                action: path_for_item(conn, item_name_singular, :update, item),
-            }
-        )
-        
-        render "form_page.html", assigns
-    end
+  def render("edit.html", assigns) do
+      conn = assigns[:conn]
+      item_name_singular = assigns[:item_name_singular]
+      item = assigns[:item]
+      item_display_name = assigns[:item_display_name]
+      
+      assigns = Map.merge(assigns, 
+          %{
+              title: "Edit " <> item_display_name,
+              back_link_title: "Back to " <> item_display_name,
+              back_link_path: path_for_item(conn, item_name_singular, :show, item),
+              action: path_for_item(conn, item_name_singular, :update, item),
+          }
+      )
+      
+      render "form_page.html", assigns
+  end
 end
