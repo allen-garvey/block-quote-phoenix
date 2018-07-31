29ae0e0838dfe66a59fe6a38010456ca6b81f33e
Allen Garvey <allen.garvey@gmail.com>
2018-07-31 15:58:07 -0400

    Change default dev port

--- config/dev.exs
+++ config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :blockquote, BlockquoteWeb.Endpoint,
-  http: [port: 3000],
+  http: [port: 6011],
   debug_errors: true,
   check_origin: false,
   code_reloader: true,
